7807a1faaad53528b4ccd821597c0f55148e814a
Glitch (picayune-fork) <none>
2019-11-16 19:23:28 +0000

    🌪🛀 Updated with Glitch

--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage(' Selamlar,Gaslı **' + username + '** Discord Sunucumuza Hoşgeldin :muscle:  #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle https://media.giphy.com/media/RlHspjp7b4PpP4yl5E/giphy.gif :wink:');
-
-};
